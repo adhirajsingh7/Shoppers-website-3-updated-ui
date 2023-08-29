6a937e0eb7aa3b813b0d333a1aa1f2a719e75bee
Adhiraj Singh <adhiraj.thakurr707@gmail.com>
2023-08-29 15:50:48 +0530

    first commit

---
+++ src/components/Additems.jsx
@@ -0,0 +1,128 @@
+import React, { useState } from 'react'
+import ItemsCard from './ItemsCard'
+import { Button, FormControl, FormControlLabel, TextField, Typography } from '@mui/material'
+import { Link } from 'react-router-dom'
+
+import '../Styles/Additems.css'
+
+const Additems = () => {
+    
+    const [cart, setCart] = useState([{
+        title : 'abc',
+        description : 'hello !!',
+        price : 0
+    },
+    {
+        title : 'xyz',
+        description : 'hello 2 !!',
+        price : 10
+    }])
+
+    const [newCart, setNewCart] = useState({
+        title : '',
+        description : '',
+        price : ''
+    })
+
+    const handleChange = (e)=>{
+        const name = e.target.name;
+        const value = e.target.value;
+        setNewCart({...newCart ,[name] : value})
+    }
+
+    const handleSubmit =(e)=>{
+        e.preventDefault()
+        console.log(newCart);
+    }
+
+    const handleStorage =(e)=>{
+
+    }
+  
+    
+    return (
+    <>
+    
+
+    <div>
+    <div className="flex-container-1">
+
+    <div className='AddItems-heading' >
+        <Typography  variant='h4'>
+        Additems
+        </Typography>
+    </div>
+      
+      
+        <div>
+          <FormControl  className="LoginForm">
+            <div >
+            <TextField  className="formItems"
+              variant="outlined"
+              label="Title"
+              type="text"
+              name="title"
+              onChange={handleChange}
+              value={newCart.title}
+            />
+            </div>
+            
+            <div>
+            <TextField  className="formItems"
+              variant="outlined"
+              label="Description"
+              type="text"
+              name="description"
+              onChange={handleChange}
+              value={newCart.description}
+            />
+            </div>
+
+
+            <div>
+            <TextField className="formItems"
+              variant="outlined"
+              label="Price"
+              type="number"
+              name="price"
+              onChange={handleChange}
+              value={newCart.price}
+            />
+            </div>
+            
+            
+
+            <div>
+            <Button className="formItems" type="submit" variant="contained" onClick={handleSubmit}>
+              Add item
+            </Button>
+            </div>
+            
+        <div>
+          <Button variant="outlined" onClick={handleStorage}>btn storage</Button>
+        </div>
+
+          </FormControl>
+        </div>
+
+      </div>
+
+    </div>
+    
+    {
+        cart.map((item ,key)=>{
+            return (
+                <div key={key}>
+                <ItemsCard  cart={item}/>
+                
+                </div>
+            )
+            
+        })
+    }
+
+    </>
+  )
+}
+
+export default Additems
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Additems from '../components/Additems'
+import Displayitems from '../components/Displayitems'
+
+const Dashboard = () => {
+  return (
+    <>
+    <Navbar/>
+    <Additems/>
+    <Displayitems/>
+    </>
+  )
+}
+
+export default Dashboard
